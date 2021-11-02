33d4d62585ab4b8cc7466ae0da3dba55311208ae
mbayelel@gmail.com <mbayelel@gmail.com>
2021-11-02 20:12:19 +0100

    updated components/Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ const Layout = ({children}) => {
     return (
         <div>
             <main>
+                <h1>Hello World</h1>
                 {children}
             </main>
         </div>
