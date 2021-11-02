68a6a909cc0841cb8a5ce7ddc0efa577fbf8c164
mbayelel@gmail.com <mbayelel@gmail.com>
2021-11-02 18:45:41 +0100

    Created component folder and added a layout component to use in pages/_app.js

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+const Layout = ({children}) => {
+    return (
+        <div>
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Next Post App</title>
+        <meta name='keywords' content='article, posts, education' />
+      </Head>
+
       <h1>Welcome to my first next app</h1>
     </div>
   )
