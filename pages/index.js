3dc5061fff1422d7257c08639d8ad1dfe691a291
mbayelel@gmail.com <mbayelel@gmail.com>
2021-11-02 18:30:37 +0100

    Modification de la page d'acceuil

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
-      <p>Welcome to my first next app</p>
+      <h1>Welcome to my first next app</h1>
     </div>
   )
 }
